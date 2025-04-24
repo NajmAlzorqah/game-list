b7e8ed843f52c8f77d12e72c62e84c5ad1805a2e
NajmAlzorqah <najmalzorqah@gmail.com>
2025-04-24 16:56:48 +0300

    Implimenting light and dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
+import { ColorModeButton } from "./ui/color-mode";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
       <Text>NavBar </Text>
+      <ColorModeButton />
     </HStack>
   );
 };
