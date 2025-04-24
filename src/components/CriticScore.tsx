95d4039ef5d950a6af95046f21793e72c23cfb85
NajmAlzorqah <najmalzorqah@gmail.com>
2025-04-24 19:40:00 +0300

    Adding the score badge

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const colorScheme = score > 75 ? "green" : score > 60 ? "yellow" : "red";
+
+  return (
+    <Badge
+      colorScheme={colorScheme}
+      fontSize="14px"
+      paddingX={2}
+      borderRadius="4px"
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
