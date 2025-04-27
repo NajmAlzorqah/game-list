f293861af84fb54788a5c50c88375c3a8ad4b13f
NajmAlzorqah <najmalzorqah@gmail.com>
2025-04-27 17:28:51 +0300

    Fixing the problem with infinit scrollin of third page

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -5,11 +5,10 @@ import {
   FaApple,
   FaLinux,
   FaAndroid,
+  FaQuestion, // Added as fallback
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
-
 import { BsGlobe, BsNintendoSwitch } from "react-icons/bs";
-
 import { Platform } from "@/hooks/usePlatforms";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
@@ -30,11 +29,19 @@ const PlatformIconList = ({ platforms }: Props) => {
     web: BsGlobe,
     android: FaAndroid,
   };
+
+  if (!platforms || platforms.length === 0) return null;
+
   return (
     <HStack marginY={1}>
-      {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
-      ))}
+      {platforms.map((platform) => {
+        if (!platform?.slug) return null;
+
+        // Use fallback icon if slug not found
+        const IconComponent = iconMap[platform.slug] || FaQuestion;
+
+        return <Icon key={platform.id} as={IconComponent} color="gray.500" />;
+      })}
     </HStack>
   );
 };
