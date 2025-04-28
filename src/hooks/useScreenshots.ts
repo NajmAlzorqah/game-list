03666f18c487ec700497d1f1ca6e4d9b44e6b025
NajmAlzorqah <najmalzorqah@gmail.com>
2025-04-29 00:19:43 +0300

    Fixing game details layout

--- src/hooks/useScreenshots.ts
+++ src/hooks/useScreenshots.ts
@@ -1,12 +1,14 @@
 import { Screenshot } from "@/entities/Screenshot";
 import APIClient from "@/services/api-client";
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 
 const useScreenshots = (gameId: number) => {
   const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
   return useQuery({
     queryKey: ["screenshots", gameId],
     queryFn: apiClient.getAll,
+    staleTime: ms("48h"),
   });
 };
 export default useScreenshots;
