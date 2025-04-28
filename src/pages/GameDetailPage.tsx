aa1d6d6889ce372831232d1a698a4d31d459b823
NajmAlzorqah <najmalzorqah@gmail.com>
2025-04-28 22:28:45 +0300

    Fetch game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,16 @@
+import useGame from "@/hooks/useGame";
+import { Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+
 const GameDetailPage = () => {
-  return <div></div>;
+  const { slug } = useParams();
+  const { data: game, error, isLoading } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
+  return <></>;
 };
 
 export default GameDetailPage;
